03744b86605c5c8eedf7a3825cafa8d073da6670
Chitsanuphongy65 <chitsanuphongy65@nu.ac.th>
2024-11-25 16:26:48 +0700

    Update question3-2.dart

--- question3-2.dart
+++ question3-2.dart
@@ -1,8 +1,14 @@
+import 'dart:io';
+
 bool isEven(int number) {
   return number % 2 == 0;
 }
 
 void main() {
-  print(isEven(4));
-  print(isEven(7));
+  print('กรุณากรอกตัวเลข:');
+  int number = int.parse(stdin.readLineSync()!);
+
+  bool result = isEven(number);
+
+  print('ผลลัพธ์: $result');
 }
