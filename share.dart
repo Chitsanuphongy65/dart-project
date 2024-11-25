eb72c29b3955b6d983ee0d98f2b9d2cf2d91c36b
Chitsanuphongy65 <chitsanuphongy65@nu.ac.th>
2024-11-25 15:01:41 +0700

    Update share.dart

--- share.dart
+++ share.dart
@@ -1,15 +1,14 @@
 import 'dart:io';
-
 void main() {
   print("กรุณาใส่ค่าอาหารทั้งหมด:");
   double totalCost = double.parse(stdin.readLineSync()!);
 
-  print("กรุณาใส่จำนวนคนทาน:");
+  print("กรุณาใส่จำนวนคน:");
   int numberOfPeople = int.parse(stdin.readLineSync()!);
 
   if (numberOfPeople > 0) {
     double costPerPerson = totalCost / numberOfPeople;
-
+    
     print("ค่าอาหารต่อคนคือ: ${costPerPerson.toStringAsFixed(2)} บาท");
   } else {
     print("จำนวนคนทานต้องมากกว่าศูนย์");
