4b65a6718bef2cd188d37b9ff32164dbef1e1d8a
Chitsanuphongy65 <chitsanuphongy65@nu.ac.th>
2024-11-25 13:16:34 +0700

    Add files via upload

---
+++ share.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+void main() {
+  print("กรุณาใส่ค่าอาหารทั้งหมด:");
+  double totalCost = double.parse(stdin.readLineSync()!);
+
+  print("กรุณาใส่จำนวนคน:");
+  int numberOfPeople = int.parse(stdin.readLineSync()!);
+
+  if (numberOfPeople > 0) {
+    double costPerPerson = totalCost / numberOfPeople;
+
+    print("ค่าอาหารต่อคนคือ: ${costPerPerson.toStringAsFixed(2)} บาท");
+  } else {
+    print("จำนวนคนทานต้องมากกว่าศูนย์");
+  }
+}
