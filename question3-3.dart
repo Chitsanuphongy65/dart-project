b99d6cb51ea8c73cf0e4b5671ba6a753719e9423
Chitsanuphongy65 <chitsanuphongy65@nu.ac.th>
2024-11-25 16:28:59 +0700

    Update question3-3.dart

--- question3-3.dart
+++ question3-3.dart
@@ -1,12 +1,22 @@
-Map<String, dynamic> createUser(String name, int age, {bool isActive = true}) {
-  return {
-    'name': name,
-    'age': age,
-    'isActive': isActive,
-  };
+import 'dart:io';
+
+void createUser(String name, int age, {bool isActive = true}) {
+  print('ชื่อ: $name');
+  print('อายุ: $age');
+  print('สถานะการใช้งาน: ${isActive ? "Active" : "Inactive"}');
 }
 
 void main() {
-  print(createUser('Alice', 25)); 
-  print(createUser('Bob', 30, isActive: false)); 
+  print('กรุณากรอกชื่อ:');
+  String name = stdin.readLineSync()!;
+
+  print('กรุณากรอกอายุ:');
+  int age = int.parse(stdin.readLineSync()!);
+
+  print('กรุณากรอกสถานะการใช้งาน (active/inactive):');
+  String status = stdin.readLineSync()!;
+
+  bool isActive = (status.toLowerCase() == 'active') ? true : false;
+
+  createUser(name, age, isActive: isActive);
 }
