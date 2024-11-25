9e09ec98b6f94bb8c16c124760c93557eda1c4dd
Chitsanuphongy65 <chitsanuphongy65@nu.ac.th>
2024-11-25 16:31:05 +0700

    Update question3-5.dart

--- question3-5.dart
+++ question3-5.dart
@@ -1,13 +1,23 @@
+import 'dart:io';
 import 'dart:math';
 
 String generateRandomPassword(int length) {
-  const String characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#\$%^&*()-_=+';
+  const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#\$%^&*()';
   Random random = Random();
-  return List.generate(length, (index) => characters[random.nextInt(characters.length)]).join();
+  String password = '';
+  
+  for (int i = 0; i < length; i++) {
+    password += characters[random.nextInt(characters.length)];
+  }
+  
+  return password;
 }
 
 void main() {
-  int passwordLength = 12; // กำหนดความยาวของรหัสผ่าน
-  String password = generateRandomPassword(passwordLength);
-  print('Generated Password: $password');
+  print('กรุณากรอกพาสเวิร์ด:');
+  int length = int.parse(stdin.readLineSync()!);
+
+  String password = generateRandomPassword(length);
+
+  print('รหัสผ่านที่สร้างขึ้น: $password');
 }
